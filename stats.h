24a769562b3cc29fee462771e2c40fe266296791
amarsareddy <66358468+amarsareddy@users.noreply.github.com>
2025-08-05 11:53:39 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -10,10 +10,10 @@ struct Stats {
 struct Stats compute_statistics(const float * numbers, int count);
 
 struct{
-  float min,
+  float min;
   float max;
   float avg;
-}
+}Stats
 
 #ifdef __cplusplus
 }
