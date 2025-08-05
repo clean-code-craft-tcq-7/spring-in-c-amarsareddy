5d94497b48fbff203f82dd2ae16afcb87f4caaee
amarsareddy <66358468+amarsareddy@users.noreply.github.com>
2025-08-05 11:45:04 +0530

    Updated C file

--- stats.c
+++ stats.c
@@ -1,5 +1,35 @@
 #include "stats.h"
 #include <stddef.h>
+#include <float.h>
 
-struct Stats compute_statistics(const * numbers, int count) {
+struct{
+  float min,
+  float max;
+  float avg;
+}
+
+struct Stats compute_statistics(const * numbers, int count)
+{
+  struct stats stats;
+  stats.min = FLT_MAX;
+  stats.max = -FLT_MAX;
+  stats.avg = 0.0f;
+  int i;
+
+  for(i=0; i<= count ;i++)
+  {
+    if(numbers[i] < stats.min)
+    {
+      stats.min = numbers[i];
+    }
+
+    if(numbers[i] > stats.max)
+    {
+      stats.max = numbers[i];
+    }
+    
+    stats.avg = stats.avg + numbers[i];
+      
+  }
+  stats.avg  = stats.avg /count;
 }
